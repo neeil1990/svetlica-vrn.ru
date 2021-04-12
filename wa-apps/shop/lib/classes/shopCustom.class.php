78f5850defa2bb1dbfd938027ab521a566f569fd
neeil1990 <neeil@mail.ru>
2021-04-12 16:11:33 +0300

    #115804 tags list for category

--- wa-apps/shop/lib/classes/shopCustom.class.php
+++ wa-apps/shop/lib/classes/shopCustom.class.php
@@ -10,4 +10,17 @@ class shopCustom
             return 'Цена от';
     }
 
+    public static function array_filter($array, $field){
+
+        if(!is_array($array) || empty($field))
+            return false;
+
+        $data = array_filter($array, function ($k) use ($field){
+
+            return preg_match("/$field/", $k);
+        }, ARRAY_FILTER_USE_KEY);
+
+        return ($data) ?: false;
+    }
+
 }
