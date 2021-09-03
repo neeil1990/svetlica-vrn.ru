4d90064b4882dd84dbc4d2abe40c85e256efaeb0
neeil1990 <neeil@mail.ru>
2021-09-03 11:18:27 +0300

    #124864 mobile menu

--- wa-data/public/site/themes/insales/js/user.js
+++ wa-data/public/site/themes/insales/js/user.js
@@ -4,4 +4,8 @@ if ( $(".category-desc").hasClass("page") ) {
 	$(".btn_to_work_wrap").hide();
 }
 
- 
+$('[data-text]').each(function(i, el){
+	let content = $(el).data('text');
+	$(el).addClass('js-text').text(content);
+});
+
